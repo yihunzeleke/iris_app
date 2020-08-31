8af5acbcfac7f80ef82e8f1054e79a3f080e8d6e
yihunzeleke <43428631+yihunzeleke@users.noreply.github.com>
2020-08-30 19:53:38 -0500

    Update init.R

--- init.R
+++ init.R
@@ -1,11 +1,12 @@
 # R code to install packages if not already installed
 
+
 my_packages = c("randomForest", "data.table")
 
-install_if_missing <- function(pckgs){
-    if (pckgs %in% rownames(install.packages()) == FALSE){
-        install.packages(pckgs)
-    }
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
 }
 
 invisible(sapply(my_packages, install_if_missing))
